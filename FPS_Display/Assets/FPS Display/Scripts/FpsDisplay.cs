eba937ce8ac341f03caba76b15aa2560e1192a3d
Truong Phu Phat Felix (Zyo) <truongphuphat98@yahoo.fr>
2019-12-06 16:54:53 +0100

    initial commit

---
+++ FPS_Display/Assets/FPS Display/Scripts/FpsDisplay.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+    [RequireComponent(typeof(Text))]
+    public class FpsDisplay : MonoBehaviour
+    {
+        [SerializeField] private float updateFrequency = 1f;
+        [SerializeField] private Text fpsText;
+
+        void Awake ()
+        {
+            fpsText = GetComponent<Text>();
+        }
+
+        void Start ()
+        {
+            StartCoroutine(UpdateCounter());
+        }
+
+        IEnumerator UpdateCounter ()
+        {
+            var waitForDelay = new WaitForSeconds(updateFrequency);
+
+            while (true)
+            {
+                var lastFrameCount = Time.frameCount;
+                var lastTime = Time.realtimeSinceStartup;
+
+                yield return waitForDelay;
+
+                var timeDelta = Time.realtimeSinceStartup - lastTime;
+                var frameDelta = Time.frameCount - lastFrameCount;
+
+                fpsText.text = string.Format("{0:0.} FPS", frameDelta / timeDelta);
+            }
+        }
+    }
